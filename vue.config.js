09baee34a9ac7ecdf0360c653de7e806a38fe145
Cameron Roe <croe@archermalmo.com>
2019-08-18 11:42:53 -0500

    Updating vue config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const MonacoEditorPlugin = require("monaco-editor-webpack-plugin");
 
 module.exports = {
+  publicPath: "javafy-your-sql",
   configureWebpack: {
     plugins: [
       new MonacoEditorPlugin({
